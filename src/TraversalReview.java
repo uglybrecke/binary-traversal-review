d7f0c0f956d3042078e3e928b8f42f9a095119a9
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-06-08 12:39:18 -0700

    positive sum method complete and tests passed

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -60,7 +60,19 @@ public class TraversalReview {
      * @return the sum of the positive values in the tree
      */
     public static int positiveSum(TreeNode node) {
-        return -1;
+        if (node == null) {
+            return 0;
+        }
+
+        int val = 0;
+        if (node.data > 0) {
+            val = node.data;
+        }
+
+        val += positiveSum(node.left);
+        val += positiveSum(node.right);
+
+        return val;
     }
 
     /**
